c9a1018a772751badc40ebe0a152bebdca099c55
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-01-16 16:45:24 +1000

    Initial commit version 1.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+# short and simple
+
+from distutils.core import setup
+
+setup(name = "PyRSS2Gen",
+      version = "1.1",
+      description = "Generate RSS2 using a Python data structure",
+      author = "Andrew Dalke",
+      author_email = "dalke@dalkescientific.com",
+      url = "http://dalkescientific.com/Python/PyRSS2Gen.html",
+      license = "BSD",
+      py_modules = ["PyRSS2Gen"])
